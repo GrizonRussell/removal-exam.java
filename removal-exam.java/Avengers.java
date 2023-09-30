72f1044a8cb9e771f266cc356c5a79ddef280b7c
gen.java <132185718+GrizonRussell@users.noreply.github.com>
2023-09-30 11:23:06 +0800

    Add files via upload

---
+++ removal-exam.java/Avengers.java
@@ -0,0 +1,11 @@
+public class Avengers{
+    String heroName = "Joss Whendon";
+    String featuredSong = "Whatever it takes";
+
+    public void showDetails(){
+    System.out.println("Be careful, not everyone who smile at you is your friend");
+    System.out.println(heroName);
+    System.out.println(featuredSong);
+
+    }
+}
---
+++ removal-exam.java/movies.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class movies{
+    public static void main(String[] args){
+        Avengers avenger = new Avengers();
+        avenger.showDetails();
+    }
+}
